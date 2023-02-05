6765598b5af5e62d4e4ef22f85535961aab22221
virijevicj <89025058+virijevicj@users.noreply.github.com>
2023-02-05 17:14:51 +0100

    Add files via upload
    dodate migracije

---
+++ database/migrations/2023_02_05_160806_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('ISBN');
+            $table->integer('year_of_publication');
+            $table->integer('grade');
+            $table->foreignId('autor_id');
+            $table->foreignId('genre_id');
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
