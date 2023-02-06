5a853190f73923996c3e7ae9ea79a485758941e8
virijevicj <89025058+virijevicj@users.noreply.github.com>
2023-02-06 16:37:29 +0100

    dodate api rute

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookAuthorController;
+use App\Http\Controllers\BookGenrerController;
+use App\Http\Controllers\BookUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,36 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//prikaz svih usera
+//Route::get('/users', [UserController::class, 'index']);
+//prikaz uzer-a po id-u
+//Route::resource('users/{id}', UserController::class);
+Route::resource('users',UserController::class);
+
+// //prikaz svih pisaca
+// Route::get('/authors', [AuthorController::class, 'index']);
+// //prikaz pisaca po id-u
+// Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::resource('authors',AuthorController::class);
+
+// //prikaz svih zanrova
+// Route::get('/genre',[GenreController::class, 'index']);
+// //prikaz zanrova po id-u
+// Route::get('/genre/{id}',[GenreController::class, 'show']);
+Route::resource('genres',GenreController::class);
+
+// //prikaz svih knjiga
+// Route::get('/books',[BookController::class, 'index']);
+// //prikaz knjige po id-u
+// Route::get('/books/{id}',[BookController::class, 'show']);
+Route::resource('books',BookController::class);
+
+//prikaz knjiga za odredjene pisce
+Route::get('books/author/{id}', [BookAuthorController::class, 'index']);
+
+//prikaz knjiga za odredjeni zanr
+Route::get('books/genre/{id}', [BookGenrerController::class, 'index']);
+
+//prikaz knjiga za odredjenog usera
+Route::get('books/user/{id}', [BookUserController::class, 'index']);
